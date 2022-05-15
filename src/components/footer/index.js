6e2e28389c60b8a56a7e7de4132961afce223d7d
MarioBatalha <mariobatalha9@gmail.com>
2022-05-15 02:24:35 +0100

    feat: add contact address

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -7,7 +7,13 @@ const Footer = () => {
   return (
     <footer>
       <div className="container">
-        
+        <div className="localization">
+          <h3>Localização</h3>
+          <p>Luanda, Bairro Rangel</p>
+          <p>Rua ARco-íris ZOna 15, casa S/N</p>
+          <p>hello@bernarinvestimentos.com</p>
+          <p>+244 953 166 250 - 931 1674 763</p>
+        </div>
       </div>
       <div className="copyright">
         <p className="copyright">
