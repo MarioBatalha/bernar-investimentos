2bdf6d80bb7e93619598e10d53753cceba3b0f6f
MarioBatalha <mariobatalha9@gmail.com>
2022-05-17 10:16:52 +0100

    feat: add whatsapp and location icons

--- src/pages/contact/index.jsx
+++ src/pages/contact/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { GoMail } from "react-icons/go";
+import { GoMail, GoLocation } from "react-icons/go";
+import { BsWhatsapp } from 'react-icons/bs';
 
 import "./style.scss";
 
@@ -13,10 +14,12 @@ const Contact = () => {
           <p>Luanda, Bairro Rangel</p>
           <p>Rua Arco-íris Zona 15, casa S/N</p>
           <p>hello@bernarinvestimentos.com</p>
+          <GoLocation size={40} color="#23395D"></GoLocation>
         </div>
         <div className="localization">
           <h3>WhatsApp</h3>
           <p>+244 953 166 250 - 931 1674 763</p>
+          <BsWhatsapp size={40} color="#23395D"></BsWhatsapp>
         </div>
         <div className="contact-form">
           <h3>Envei-nos um email</h3>
@@ -27,7 +30,7 @@ const Contact = () => {
           </p>
 
           <a href="mailto: mcbattle98@gmail.com">
-            <GoMail size={40} color="dark-blue"></GoMail>
+            <GoMail size={40} color="#23395D"></GoMail>
           </a>
         </div>
       </div>
