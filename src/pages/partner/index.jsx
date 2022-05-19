66a75df610eb2557e87f84f43c48c806479f2d6d
MarioBatalha <mariobatalha9@gmail.com>
2022-05-19 12:17:04 +0100

    feat: animate css configurations

--- src/pages/partner/index.jsx
+++ src/pages/partner/index.jsx
@@ -13,67 +13,68 @@ import { SiDell,
   SiMailchimp
 } from 'react-icons/si';
 import "./style.scss";
+import 'animate.css';
 
 const partners = [
   {
     id: 1,
-    url: <BsApple size={90} color="hsl(205, 72%, 37%)" className="icon" />,
+    url: <BsApple size={90} className="icon"/>,
   },
   {
     id: 2,
-    url: <ImAndroid size={90} color="hsl(205, 72%, 37%)" />,
+    url: <ImAndroid size={90} className="icon" />,
   },
   {
     id: 3,
-    url: <BsMicrosoft size={90} color="hsl(205, 72%, 37%)" />,
+    url: <BsMicrosoft size={90} className="icon" />,
   },
   {
     id: 4,
-    url: <BsGoogle size={90} color="hsl(205, 72%, 37%)" />,
+    url: <BsGoogle size={90} className="icon" />,
   },
   {
     id: 5,
-    url: <SiToshiba size={90} color="hsl(205, 72%, 37%)" />,
+    url: <SiToshiba size={90} className="icon" />,
   },
   {
     id: 6,
-    url: <SiCisco size={90} color="hsl(205, 72%, 37%)" />,
+    url: <SiCisco size={90} className="icon" />,
   },
   {
     id: 7,
-    url: <SiDell size={90} color="hsl(205, 72%, 37%)" />,
+    url: <SiDell size={90} className="icon" />,
   },
   {
     id: 8,
-    url: <SiLenovo size={90} color="hsl(205, 72%, 37%)" />,
+    url: <SiLenovo size={90} className="icon" />,
   },
   {
     id: 9,
-    url: <SiSamsung size={90} color="hsl(205, 72%, 37%)" />,
+    url: <SiSamsung size={90} className="icon" />,
   },
   {
     id: 10,
-    url: <SiEpson size={90} color="hsl(205, 72%, 37%)"/>,
+    url: <SiEpson size={90} className="icon" />,
   },
   {
     id: 11,
-    url: <SiAsus size={90} color="hsl(205, 72%, 37%)" />,
+    url: <SiAsus size={90}  className="icon" />,
   },
   {
     id: 12,
-    url: <SiPlaystation size={90} color="hsl(205, 72%, 37%)"/>,
+    url: <SiPlaystation size={90} className="icon" />,
   },
   {
     id: 13,
-    url: <ImAmazon size={90} color="hsl(205, 72%, 37%)" />,
+    url: <ImAmazon size={90} className="icon" />,
   },
   {
     id: 14,
-    url: <SiAdobecreativecloud size={90} color="hsl(205, 72%, 37%)"/>,
+    url: <SiAdobecreativecloud size={90} className="icon" />,
   },
   {
     id: 15,
-    url: <SiMailchimp size={90} color="hsl(205, 72%, 37%)"/>,
+    url: <SiMailchimp size={90} className="icon" />,
   },
 ];
 
@@ -86,7 +87,7 @@ const Partner = () => {
             const { id, url } = item;
             return (
                 <div className="partner-description" key={id} >
-                  {url}
+                  <span className="animate__animated animate__bounce">{url}</span>
                 </div>
             )
         })}
